e9b26a6709c04694c9cf9bbc02728efcc81e44cf
Rackyrr <valentin.lestrelin@free.fr>
2025-02-10 16:13:03 +0100

    Fix : migrations files

--- migrations/Version20250210151127.php
+++ migrations/Version20250210151127.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250203160317 extends AbstractMigration
+final class Version20250210151127 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20250203160317 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE country ADD country_code VARCHAR(8) NOT NULL');
+        $this->addSql('CREATE TABLE country (country_code VARCHAR(8) NOT NULL, name VARCHAR(255) NOT NULL, url_flag VARCHAR(255) NOT NULL, PRIMARY KEY(country_code)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE country DROP country_code');
+        $this->addSql('DROP TABLE country');
     }
 }
